bc9a7b74ebd5fcde7a1d38f122ceb79993131054
sandhi-artha <sandiartha99@yahoo.co.id>
2020-05-30 09:33:23 +0200

    attempting to rearrange drawFaceBlobs function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Face from './Components/Face/Face';
 import Nav from './Components/Nav/Nav';
 import Signin from './Components/User/Signin';
 import Register from './Components/User/Register';
+import Profile from './Components/Profile/Profile';
 
 
 const initialState = {
@@ -33,7 +34,7 @@ class App extends React.Component{
       })
       .then(resp => resp.json())
       .then(response => {
-        document.querySelector(".face-image").src = server+response.images[0].imgurl;
+        document.querySelector(".face-image").src = server+response.images[0].imgurl;   // set to static url in server where the image was downloaded
         this.setState({
           currPredictions: response.predictions,
           userImages: this.state.userImages.concat(response.images),
@@ -46,6 +47,7 @@ class App extends React.Component{
 
     const onRouteChange = (newRoute) => {
       if(newRoute==='signin'){                                    // if returning to signin view (means user is signing out)
+        revokeStateURL()                                          // revoke prev objectURL in state
         document.querySelector('.nav').classList.add('hidden');   // hide the signout nav
         this.setState(initialState);                              // reset to initialState
       }
@@ -92,10 +94,10 @@ class App extends React.Component{
           canvas.width = canvas.height = targetImageSize;
           ctx.drawImage(imgObj, sx, sy, sWidth, sHeight, 0, 0, targetImageSize, targetImageSize);
           canvas.toBlob(blob => {     // .toBlob doesn't return immediate value, but the blob will show in a callback once it finish loading (async)
-            blobFile.push(blob);
+            blobFile.push(blob);      // store each blob in an array
             blobCount++;
-            // if(blobCount === predicts.length){ sendPrediction(blobFile) }   // store blob in array and triggers the send function without when all blobs completed
             if(blobCount === predicts.length){   // when all the blobs have been collected
+              revokeStateURL();
               this.setState({ blobURL: blob2imgArr(blobFile)});
               console.log("sending blobs..")
               sendPrediction(blobFile);
@@ -105,13 +107,27 @@ class App extends React.Component{
       }
     }
 
-    // convert blobs into objectURL
-    const blob2imgArr = (blobs) => {
-      // state.forEach(url => URL.revokeObjectURL(url))} 
-      return blobs.map(blob => URL.createObjectURL(blob))
+    const onClickProfileImg = (imgid) => {
+      // if the first thing a user does is clicking profile image, then the FaceImage comp needs to show up
+      // document.querySelector('.face').classList.remove('hidden');
+      const imgObj = document.querySelector(".face-image");
+      // if user previously predicts an image, the main img will have onload function that draws blob elements, profile images uses saved
+      imgObj.removeAttribute("onload");
+      // find the selected image using imgid
+      const images = this.state.userImages.filter(image => image.imgid === imgid);
+      imgObj.src = server+images[0].imgurl;
+      // grab predictions related to the imgid and map the data to state.predict, and local url to state.blobUrl
+      const predictions = this.state.userPredictions.filter(predict => predict.imgid === imgid);
+      this.setState({currPredictions: predictions});
     }
 
-    const {currPredictions, blobURL} = this.state;
+    // revoke previous objectURL
+    const revokeStateURL = () => { this.state.blobURL.forEach(url => URL.revokeObjectURL(url)) }
+
+    //  convert new blobs into objectURL
+    const blob2imgArr = (blobs) => blobs.map(blob => URL.createObjectURL(blob))
+
+    const {currPredictions, blobURL, userImages} = this.state;
     return (
       <div className="App">
         <h1>Welcome!</h1>
@@ -125,6 +141,7 @@ class App extends React.Component{
                   <Search geturl={geturl} getState={getState} onButtonSubmit={onButtonSubmit}/>
                   <Face currPredictions={currPredictions} blobURL={blobURL}/>
                   <canvas id='myCanvas'></canvas>
+                  <Profile userImages={userImages} onClickProfileImg={onClickProfileImg} server={server}/>
                 </div>
         }
         
@@ -146,14 +163,17 @@ export default App;
   8. draw faceblobs using canvas and convert to blobURL   DONE
   9. show the blobURLs in face-info   DONE
   10. show prediction results in face-info  DONE
-  11. remember to revoke object url
+  11. remember to revoke object url   DONE
 
   focus: limiting the image/canvas size (css) so the blobs are correct
          dealing with CORS policy, remove crossOrigin in img el, instead of fetching the img from original source, get it from the downloaded server
          bug, on first predict, drawFaceBlobs was not executed. possible sol: setState is asnyc, so the state hasn't been update yet when drawFaceBlobs was executed
 
 
-  12. create mockup data to simulate history images
-  13. create history and its card components
-  14. when user clicks on a history image, it will sent that data to currPrediction
+  12. create profile and its card components  DONE
+  13. when user clicks on a profile image, it will sent that data to currPrediction   DONE
+  14. show faceblobs of a profile image, use drawFaceBlobs, and don't revoke the object url yet, use it if later the user clicks an image again
+      - rearrange drawFaceBlobs to fit in the Face components, maybe
+      - create userBlobs state, store a blob with it's predid
+      - only revoke object url on signout
 */
