e89020ea5b9983c9a7dc4e7758babb31983eac85
sandhi-artha <sandiartha99@yahoo.co.id>
2020-06-01 14:51:48 +0200

    Updates

---
+++ precache-manifest.a06d13e7d4d17e39dd70fa2cdbb50504.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6483b669140b9865f9162d99c8d70da",
+    "url": "/face-demographic-detection/index.html"
+  },
+  {
+    "revision": "15257d58ae5ae00b38c2",
+    "url": "/face-demographic-detection/static/css/main.2f41c2bb.chunk.css"
+  },
+  {
+    "revision": "73ab5e7ea2aa270bb05a",
+    "url": "/face-demographic-detection/static/js/2.20398e68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/face-demographic-detection/static/js/2.20398e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15257d58ae5ae00b38c2",
+    "url": "/face-demographic-detection/static/js/main.6fb744e9.chunk.js"
+  },
+  {
+    "revision": "68345266bc8602727618",
+    "url": "/face-demographic-detection/static/js/runtime-main.7415fb5f.js"
+  }
+]);
